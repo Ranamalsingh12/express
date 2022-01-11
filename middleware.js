c0da68201c730e896667627e33ce78f55b022de7
ranamalsingh12 <ranams99911@gmail.com>
2022-01-11 11:25:20 +0530

    middle ware

--- middleware.js
+++ middleware.js
@@ -4,3 +4,17 @@ const myLogger = function (req, res, next) {
     console.log("Logged");
     next();
 }
+
+const reqTime = function (req, res, next) {
+    req.reqTime = Date.now();
+    next();
+}
+
+app.use(myLogger);
+app.use(reqTime);
+
+app.get('/',function (req, res) {
+    res.send(`Current Time: ${req.reqTime}`);
+})
+
+app.listen(3000, ()=>{console.log("App started at 3000");})
