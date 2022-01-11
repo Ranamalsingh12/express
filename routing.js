be5d285545ce4e2e0e3ed5e77c4546abed61358a
ranamalsingh12 <ranams99911@gmail.com>
2022-01-11 12:06:41 +0530

    routing

---
+++ routing.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+const data = {
+    id :1,
+    name : "India"
+}
+
+app.get('/', function(req, res){
+    res.end("Welcome to my home page")
+})
+
+app.get('/about', (req ,res) =>  {
+    res.send("welcome to my about page")
+})
+
+app.get('/weather', (req ,res) =>  {
+    res.send(data)
+})
+
+app.listen(port, ()=>{console.log("server is started on port",port)})
